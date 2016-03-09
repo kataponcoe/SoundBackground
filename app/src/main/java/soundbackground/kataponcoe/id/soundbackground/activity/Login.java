ebcd79e4b710912485d755cfd0bb2035944447ed
Poncoe <Poncoe@Poncoes-MacBook.local>
2016-03-09 20:18:51 +0700

    login

--- app/src/main/java/soundbackground/kataponcoe/id/soundbackground/activity/Login.java
+++ app/src/main/java/soundbackground/kataponcoe/id/soundbackground/activity/Login.java
@@ -1,7 +1,18 @@
 package soundbackground.kataponcoe.id.soundbackground.activity;
 
+import android.app.Activity;
+import android.os.Bundle;
+
+import soundbackground.kataponcoe.id.soundbackground.R;
+
 /**
  * Created by Poncoe on 09/03/16.
  */
-public class Login {
+public class Login extends Activity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_activity);
+    }
 }
--- app/src/main/java/soundbackground/kataponcoe/id/soundbackground/activity/SplashScreen.java
+++ app/src/main/java/soundbackground/kataponcoe/id/soundbackground/activity/SplashScreen.java
@@ -1,4 +1,41 @@
 package soundbackground.kataponcoe.id.soundbackground.activity;
 
-public class SplashScreen {
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+import soundbackground.kataponcoe.id.soundbackground.R;
+
+public class SplashScreen extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_main);
+
+        Thread logoTimer = new Thread() {
+            public void run(){
+                try{
+                    int logoTimer = 0;
+                    while(logoTimer < 6000){
+                        sleep(100);
+                        logoTimer = logoTimer +100;
+                    };
+                    startActivity(new Intent(SplashScreen.this, Login.class));
+                    finish();
+                }
+
+                catch (InterruptedException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+
+                finally{
+                    finish();
+                }
+            }
+        };
+
+        logoTimer.start();
+    }
 }
